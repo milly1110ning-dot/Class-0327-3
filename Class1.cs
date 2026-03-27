b6f483379b1e2872e45292c1d0d1c1e7f39ef2c1
milly1110ning-dot <milly.1110ning@gmail.com>
2026-03-27 10:59:59 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+public class Test
+{
+    private int Mid;
+    private int Final;
+
+    public Test(int Mid, int Final)
+    {
+        this.Mid = Mid;
+        this.Final= Final;
+    }
+
+    public void SetMid(int Mid)
+    {
+       this.Mid = Mid;
+    }
+    public void SetFinal(int Final)
+    {
+       this.Final = Final;
+    }
+    public int GetMid()
+    {
+        return Mid;
+    }
+    public int GetFinal()
+    {
+        return Final;
+    }
+    public double GetAvg()
+    {
+        return (Mid+Final)/2.0;
+    }
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Class_0327_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Test std1 = new Test(78, 72);
+            Test std2 = new Test(5, 5);
+
+            std1.SetMid(88);
+            std1.SetFinal(82);
+
+
+
+
+            Console.WriteLine("1:"+std1.GetAvg().ToString());
+            Console.WriteLine("2:"+std2.GetFinal().ToString());
+
+        }
+    }
+}
